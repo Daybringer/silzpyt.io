739197a4e7082d0e7fd92b672b5d484f939b4ba0
Daybringer <vanata.michal@gmail.com>
2019-12-26 00:52:14 +0100

    working alpha

--- mechanics/canon.js
+++ mechanics/canon.js
@@ -1,157 +1,129 @@
-const GRAVITY = 300;
-
+// let game_height = window.innerHeight * window.devicePixelRatio - 50;
+let game_height = document.getElementById("phaser_game").offsetHeight;
+let game_width = document.getElementById("phaser_game").offsetWidth;
 var config = {
-  width: window.innerWidth * window.devicePixelRatio,
-  height: window.innerHeight * window.devicePixelRatio,
   type: Phaser.AUTO,
+  // width: window.innerWidth * window.devicePixelRatio,
+  // height: window.innerHeight * window.devicePixelRatio,
   parent: "phaser_game",
-  title: "Canon",
-  version: "0.1",
-  fps: 30,
+  height: game_height,
+  width: game_width,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: GRAVITY },
+      gravity: { y: 300 },
       debug: false
     }
   },
   scene: {
-    preload: onPreload,
-    create: onCreate,
-    update: onUpdate,
-    resize: onResize
-  }
+    preload: preload,
+    create: create,
+    update: update
+  },
+  fps: 30
 };
 
-// var player;
-// var stars;
-// var bombs;
-// var platforms;
-// var cursors;
-// var score = 0;
-// var gameOver = false;
-// var scoreText;
+let block;
+let platform;
+let cursors;
+let velocity;
 
 var game = new Phaser.Game(config);
 
-function onPreload() {
-  //   this.load.image("sky", "assets/sky.png");
-  //   this.load.image("ground", "assets/platform.png");
-  //   this.load.image("star", "assets/star.png");
-  //   this.load.image("bomb", "assets/bomb.png");
-  //   this.load.spritesheet("dude", "assets/dude.png", {
-  //     frameWidth: 32,
-  //     frameHeight: 48
-  //   });
+function preload() {
+  this.load.image("canon", "assets/canon.png");
+  this.load.image("canon_ball", "assets/ball.png");
+  this.load.image("platform", "assets/platform.png");
+  this.load.image("canon_stand", "assets/canon_stand.png");
+  this.load.image("block", "assets/block.png");
 }
 
-function onCreate() {
-  //   //  A simple background for our game
-  //   this.add.image(400, 300, "sky");
-  //   //  The platforms group contains the ground and the 2 ledges we can jump on
-  //   platforms = this.physics.add.staticGroup();
-  //   //  Here we create the ground.
-  //   //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  //   platforms
-  //     .create(400, 568, "ground")
-  //     .setScale(2)
-  //     .refreshBody();
-  //   //  Now let's create some ledges
-  //   platforms.create(600, 400, "ground");
-  //   platforms.create(50, 250, "ground");
-  //   platforms.create(750, 220, "ground");
-  //   // The player and its settings
-  //   player = this.physics.add.sprite(100, 450, "dude");
-  //   //  Player physics properties. Give the little guy a slight bounce.
-  //   player.setBounce(0.2);
-  //   player.setCollideWorldBounds(true);
-  //   //  Our player animations, turning, walking left and walking right.
-  //   this.anims.create({
-  //     key: "left",
-  //     frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-  //     frameRate: 10,
-  //     repeat: -1
-  //   });
-  //   this.anims.create({
-  //     key: "turn",
-  //     frames: [{ key: "dude", frame: 4 }],
-  //     frameRate: 20
-  //   });
-  //   this.anims.create({
-  //     key: "right",
-  //     frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-  //     frameRate: 10,
-  //     repeat: -1
-  //   });
-  //   //  Input Events
-  //   cursors = this.input.keyboard.createCursorKeys();
-  //   //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-  //   stars = this.physics.add.group({
-  //     key: "star",
-  //     repeat: 11,
-  //     setXY: { x: 12, y: 0, stepX: 70 }
-  //   });
-  //   stars.children.iterate(function(child) {
-  //     //  Give each star a slightly different bounce
-  //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  //   });
-  //   bombs = this.physics.add.group();
-  //   //  The score
-  //   scoreText = this.add.text(16, 16, "score: 0", {
-  //     fontSize: "32px",
-  //     fill: "#000"
-  //   });
-  //   //  Collide the player and the stars with the platforms
-  //   this.physics.add.collider(player, platforms);
-  //   this.physics.add.collider(stars, platforms);
-  //   this.physics.add.collider(bombs, platforms);
-  //   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-  //   this.physics.add.overlap(player, stars, collectStar, null, this);
-  //   this.physics.add.collider(player, bombs, hitBomb, null, this);
+function create() {
+  // Background
+  let { width, height } = this.sys.game.canvas;
+  this.cameras.main.backgroundColor.setTo(255, 255, 255);
+  //
+  //Ground platform
+  platform = this.physics.add.staticGroup();
+  platform
+    .create(0, game_height - 32, "platform")
+    .setScale(8, 2)
+    .refreshBody();
+  //
+  //Blocks
+  // 34x34 size >> 34x126
+  blocks = this.physics.add.group();
+  blocks
+    .create(
+      game_width - 0.2 * game_width,
+      game_height - 0.3 * game_height,
+      "block"
+    )
+    .setScale(1, 3);
+  blocks
+    .create(
+      game_width - 0.2 * game_width - 38,
+      game_height - 0.3 * game_height,
+      "block"
+    )
+    .setScale(1, 3);
+  blocks
+    .create(
+      game_width - 0.2 * game_width - 18,
+      game_height - 0.5 * game_height,
+      "block"
+    )
+    .setScale(1, 3);
+  //
+  // Canon stand
+  canon_stand = this.physics.add.staticGroup();
+  canon_stand
+    .create(
+      game_width - 0.9 * game_width,
+      game_height - 0.15 * game_height,
+      "canon_stand"
+    )
+    .setScale(1, 1);
+  //
+  // Canon
+  canon = this.physics.add.sprite(
+    game_width - 0.86 * game_width,
+    game_height - 0.25 * game_height,
+    "canon"
+  );
+  //
+  // Canon ball
+  //
+  //
+  canon.body.setAllowGravity(false);
+  this.physics.add.collider(blocks, platform);
+  this.physics.add.collider(blocks, blocks);
+
+  cursors = this.input.keyboard.createCursorKeys();
+  spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 }
 
-function onUpdate() {
-  //   if (gameOver) {
-  //     return;
-  //   }
-  //   if (cursors.left.isDown) {
-  //     player.setVelocityX(-160);
-  //     player.anims.play("left", true);
-  //   } else if (cursors.right.isDown) {
-  //     player.setVelocityX(160);
-  //     player.anims.play("right", true);
-  //   } else {
-  //     player.setVelocityX(0);
-  //     player.anims.play("turn");
-  //   }
-  //   if (cursors.up.isDown && player.body.touching.down) {
-  //     player.setVelocityY(-330);
-  //   }
+function update() {
+  // if (Phaser.Input.Keyboard.JustDown(spacebar)) {
+  //   pointer => {
+  //     canon_ball.create(pointer.x, pointer.y, "canon_ball").setScale(1);
+  //   };
   // }
-  // function collectStar(player, star) {
-  //   star.disableBody(true, true);
-  //   //  Add and update the score
-  //   score += 10;
-  //   scoreText.setText("Score: " + score);
-  //   if (stars.countActive(true) === 0) {
-  //     //  A new batch of stars to collect
-  //     stars.children.iterate(function(child) {
-  //       child.enableBody(true, child.x, 0, true, true);
-  //     });
-  //     var x =
-  //       player.x < 400
-  //         ? Phaser.Math.Between(400, 800)
-  //         : Phaser.Math.Between(0, 400);
-  //     var bomb = bombs.create(x, 16, "bomb");
-  //     bomb.setBounce(1);
-  //     bomb.setCollideWorldBounds(true);
-  //     bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-  //     bomb.allowGravity = false;
-  //   }
-  // }
-  // function hitBomb(player, bomb) {
-  //   this.physics.pause();
-  //   player.setTint(0xff0000);
-  //   player.anims.play("turn");
-  //   gameOver = true;
+
+  this.input.on(
+    "pointerdown",
+    function(pointer) {
+      canon_ball = this.physics.add.sprite(
+        game_width - 0.86 * game_width,
+        game_height - 0.25 * game_height,
+        "canon_ball"
+      );
+      this.physics.moveTo(canon_ball, pointer.x, pointer.y, 500);
+      this.physics.add.collider(blocks, canon_ball);
+      this.physics.add.collider(canon_ball, platform);
+      this.physics.add.collider(canon_ball, canon_ball);
+    },
+    this
+  );
+  // this.physics.moveTo(bullet, cursor.x, cursor.y);
 }
